3730dac5fc6768771650986251bf4463d9703f00
Beth Njuguna <libbybetha@gmail.com>
2022-10-26 13:12:38 +0300

    completed assignment

---
+++ lib/user.rb
@@ -0,0 +1,22 @@
+class User
+        attr_writer :name
+    @knowledge = []
+    def first_name=(first_name)
+    @first_name= first_name
+     end
+
+     attr_reader :name
+      def first_name
+       @first_name
+      end
+      attr_writer :name
+      def last_name=(last_name )
+      @last_name= last_name
+       end
+  
+       attr_reader :name
+        def last_name
+         @last_name
+        end
+
+end
